0156792017e80340f529760fe81eb4322a0d1453
Arthur Fenili <arthur.fenili@hotmail.com>
2025-02-11 09:55:06 -0300

    add model e repository

---
+++ src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.spring_mvc.repository;
+
+import br.com.fiap.spring_mvc.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
