a75a6c8a0f293f630ed004cf063d5a2e00820516
Arthur <arthur.fenili@hotmail.com>
2025-02-14 09:49:54 -0300

    commit - 14/02

--- src/main/java/br/com/fiap/spring_mvc/model/Livro.java
+++ src/main/java/br/com/fiap/spring_mvc/model/Livro.java
@@ -2,11 +2,13 @@ package br.com.fiap.spring_mvc.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @Entity
-@Table(name = "mvc_livro")
 @Data
+@Table(name = "mvc_livro")
 public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
